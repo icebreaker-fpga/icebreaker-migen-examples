43f04eeab45406ca2833e28c104069a7741e1587
Piotr Esden-Tempski <piotr@esden.net>
2018-11-13 15:16:11 -0800

    Fixed the example, now it should be doing the right thing. ;)

--- pwm_fade/pwm_fade.py
+++ pwm_fade/pwm_fade.py
@@ -13,21 +13,64 @@ class UpdownCounter(Module):
     def __init__(self, counter, bitwidth):
         icounter = Signal(bitwidth+1)
         direction = Signal()
+
         self.comb += direction.eq(icounter[bitwidth])
-        self.sync += If(direction,
+        self.comb += If(direction,
                         counter.eq(~icounter[0:bitwidth])
                         ).Else(
                         counter.eq( icounter[0:bitwidth]))
-        self.sync += icounter.eq(icounter + 1)
+
+        icounter_inv = Signal(bitwidth)
+        self.comb += icounter_inv.eq(~icounter[0:bitwidth])
+        self.sync += If(icounter_inv == 0,
+                            icounter.eq(icounter + 2)
+                        ).Else(
+                            icounter.eq(icounter + 1))
+
+class TickUpdownCounter(Module):
+    def __init__(self, counter, tick, bitwidth):
+        icounter = Signal(bitwidth+1)
+        direction = Signal()
+
+        self.comb += direction.eq(icounter[bitwidth])
+        self.comb += If(direction,
+                        counter.eq(~icounter[0:bitwidth])
+                        ).Else(
+                        counter.eq( icounter[0:bitwidth]))
+
+        icounter_inv = Signal(bitwidth)
+        self.comb += icounter_inv.eq(~icounter[0:bitwidth])
+        self.sync += If(tick,
+                        If((icounter_inv) == 0,
+                            icounter.eq(icounter + 2)
+                        ).Else(
+                            icounter.eq(icounter + 1)))
+
+class ClockDiv(Module):
+    def __init__(self, divbitwidth, divout, divtick):
+        divcounter = Signal(divbitwidth+1)
+        # count every clock tick
+        self.sync += divcounter.eq(divcounter + 1)
+        # output 50% duty cycle clock output
+        self.comb += divout.eq(divcounter[divbitwidth])
+        # output a one clock wide strobe
+        divcounter_inv = Signal(divbitwidth)
+        self.comb += divcounter_inv.eq(~divcounter[0:divbitwidth])
+        self.comb += divtick.eq(divcounter_inv == 0)
 
 class PWMFade(Module):
     def __init__(self, pwm_signal, dbg, width, div):
         pwm_value = Signal(width)
         self.submodules.pwm = PWM(pwm_signal, width, pwm_value)
-        counter = Signal(width+div)
-        self.submodules.updown = UpdownCounter(counter, width+div)
-        self.comb += pwm_value.eq(counter[div:width+div])
-        self.comb += dbg.eq(counter[7])
+
+        updown_clock = Signal()
+        updown_clock_strobe = Signal()
+        self.submodules.updown_clk_div = \
+            ClockDiv(div, updown_clock, updown_clock_strobe)
+
+        self.submodules.updown = \
+            TickUpdownCounter(pwm_value, updown_clock_strobe, width)
+        self.comb += dbg.eq(updown_clock)
 
 def _test(dut):
     for i in range(1000):
